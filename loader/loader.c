365c49910516af47e573f2314ebed16d23662a0b
pascal <baesae@hotmail.ch>
2014-10-17 12:34:13 +0200

    Started binary loader

---
+++ loader/loader.c
@@ -0,0 +1,14 @@
+/*
+ * loader.c
+ *
+ *  Created on: 17.10.2014
+ *      Author: pascal
+ */
+
+
+#include "loader.h"
+
+
+void loader_load(FILE *fp)
+{
+}
---
+++ loader/loader.h
@@ -0,0 +1,15 @@
+/*
+ * loader.h
+ *
+ *  Created on: 17.10.2014
+ *      Author: pascal
+ */
+
+#ifndef LOADER_H_
+#define LOADER_H_
+
+#include "stdio.h"
+
+void loader_load(FILE *fp);
+
+#endif /* LOADER_H_ */
