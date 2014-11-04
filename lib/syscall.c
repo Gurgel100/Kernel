4f64e115da8cae1dc01fe6879a38b6d202cf99ad
pascal <baesae@hotmail.ch>
2014-11-04 13:43:14 +0100

    Removed not required structure

--- lib/syscall.c
+++ lib/syscall.c
@@ -39,33 +39,33 @@ inline void __attribute__((noreturn)) syscall_exit(int status)
 	while(1);
 }
 
-inline void *syscall_fopen(char *path, mode_t mode)
+inline void *syscall_fopen(char *path, vfs_mode_t mode)
 {
 	void *ret;
-	asm volatile("int $0x30" : "=a"(ret) : "a"(40), "b"(path), "c"(mode));
+	asm volatile("int $0x30" : "=a"(ret) : "a"(40), "b"(path), "c"(&mode));
 	return ret;
 }
 
-inline void syscall_fclose(vfs_stream_t *stream)
+inline void syscall_fclose(void *stream)
 {
 	asm volatile("int $0x30" : : "a"(41), "b"(stream));
 }
 
-inline size_t syscall_fread(vfs_stream_t *stream, uint64_t start, size_t length, const void *buffer)
+inline size_t syscall_fread(void *stream, uint64_t start, size_t length, const void *buffer)
 {
 	size_t ret;
-	asm volatile("int $0x30" : "=a"(ret) : "a"(42), "b"(stream), "c"(start), "d"(length), "e"(buffer));
+	asm volatile("int $0x30" : "=a"(ret) : "a"(42), "b"(stream), "c"(start), "d"(length), "D"(buffer));
 	return ret;
 }
 
-inline size_t syscall_fwrite(vfs_stream_t *stream, uint64_t start, size_t length, const void *buffer)
+inline size_t syscall_fwrite(void *stream, uint64_t start, size_t length, const void *buffer)
 {
 	size_t ret;
-	asm volatile("int $0x30" : "=a"(ret) : "a"(43), "b"(stream), "c"(start), "d"(length), "e"(buffer));
+	asm volatile("int $0x30" : "=a"(ret) : "a"(43), "b"(stream), "c"(start), "d"(length), "D"(buffer));
 	return ret;
 }
 
-inline uint64_t syscall_StreamInfo(vfs_stream_t *stream, vfs_fileinfo_t info)
+inline uint64_t syscall_StreamInfo(void *stream, vfs_fileinfo_t info)
 {
 	uint64_t ret;
 	asm volatile("int $0x30" : "=a"(ret) : "a"(44), "b"(stream), "c"(info));
--- lib/syscall.h
+++ lib/syscall.h
@@ -12,18 +12,11 @@
 
 #include "stddef.h"
 #include "stdint.h"
+#include "stdbool.h"
 
 typedef struct{
 	bool read, write, append, empty, create;
 }vfs_mode_t;
-
-typedef struct{
-	struct cdi_fs_stream stream;
-	uint64_t id;
-	vfs_mode_t mode;
-
-	vfs_node_t *node;
-}vfs_stream_t;
 
 typedef enum{
 	VFS_INFO_FILESIZE, VFS_INFO_BLOCKSIZE, VFS_INFO_USEDBLOCKS, VFS_INFO_CREATETIME, VFS_INFO_ACCESSTIME, VFS_INFO_CHANGETIME
@@ -37,11 +30,11 @@ inline void syscall_putch(unsigned char c);
 
 inline void syscall_exit(int status);
 
-inline vfs_stream_t *syscall_fopen(char *path, vfs_mode_t mode);
-inline void syscall_fclose(vfs_stream_t *stream);
-inline size_t syscall_fread(vfs_stream_t *stream, uint64_t start, size_t length, const void *buffer);
-inline size_t syscall_fwrite(vfs_stream_t *stream, uint64_t start, size_t length, const void *buffer);
-inline uint64_t syscall_StreamInfo(vfs_stream_t *stream, vfs_fileinfo_t info);
+inline void *syscall_fopen(char *path, vfs_mode_t mode);
+inline void syscall_fclose(void *stream);
+inline size_t syscall_fread(void *stream, uint64_t start, size_t length, const void *buffer);
+inline size_t syscall_fwrite(void *stream, uint64_t start, size_t length, const void *buffer);
+inline uint64_t syscall_StreamInfo(void *stream, vfs_fileinfo_t info);
 
 #endif /* SYSCALL_H_ */
 
