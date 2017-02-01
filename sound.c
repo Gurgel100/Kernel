8f8dbb554fef49909f027af0d3683dc8b0fc83bb
Gurgel100 <baesae@hotmail.ch>
2017-02-01 21:06:03 +0100

    Moved function declaration

--- sound.c
+++ sound.c
@@ -13,6 +13,16 @@
 
 #define PIT_FRQ	1193182
 
+static void switch_on()
+{
+	outb(0x61, inb(0x61) | 0x3);
+}
+
+static void switch_off()
+{
+	outb(0x61, inb(0x61) & ~0x3);
+}
+
 void sound_Play(uint32_t freq, uint64_t sleep)
 {
 	pit_InitChannel(2, 3, PIT_FRQ / freq);
@@ -20,16 +30,6 @@ void sound_Play(uint32_t freq, uint64_t sleep)
 	switch_on();
 	Sleep(sleep);
 	switch_off();
-}
-
-void switch_on()
-{
-	outb(0x61, inb(0x61) | 0x3);
-}
-
-void switch_off()
-{
-	outb(0x61, inb(0x61) & ~0x3);
 }
 
 #endif
