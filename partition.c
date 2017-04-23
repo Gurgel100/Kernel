3941132a04c04dd60761441a269a899486f5b85b
Gurgel100 <baesae@hotmail.ch>
2017-04-23 18:07:12 +0200

    Hidden some internal structures

--- partition.c
+++ partition.c
@@ -12,13 +12,52 @@
 #include "scsi.h"
 #include "fs.h"
 #include "lists.h"
-#include "vfs.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 #include "drivermanager.h"
 
 #define MIN(val1, val2) ((val1 < val2) ? val1 : val2)
+
+typedef enum{
+	PART_TYPE_NONE = 0x00,
+	PART_TYPE_LINUX = 0x83,
+	PART_TYPE_ISO9660 = 0xCD,
+	PART_TYPE_LAST = 0xFF
+}__attribute__((packed))part_type_t;
+
+typedef struct{
+	struct{
+		uint8_t Bootflag;
+		uint8_t Head;
+		uint8_t Sector : 6;
+		uint8_t CylinderHi : 2;
+		uint8_t CylinderLo;
+		part_type_t Type;
+		uint8_t lastHead;
+		uint8_t lastSector : 6;
+		uint8_t lastCylinderHi : 2;
+		uint8_t lastCylinderLo;
+		uint32_t firstLBA;
+		uint32_t Length;
+	}__attribute__((packed))entry[4];
+}__attribute__((packed))PartitionTable_t;
+
+typedef struct{
+	uint8_t sector;
+	uint8_t head;
+	uint8_t cylinder;
+}chs_t;
+
+typedef struct{
+	uint64_t id;
+	char *name;
+	vfs_device_t *vfs_dev;
+	vfs_file_t dev_stream;
+	vfs_filesystem_t *fs;
+	size_t lbaStart, lbaSize, blocksize;
+	part_type_t type;
+}partition_t;
 
 static const char *fs_drivers[] = {
 [PART_TYPE_LINUX]	"ext2",
