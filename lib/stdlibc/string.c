9b55ca108310b32a6536e75444c1a3cec635fd50
Gurgel100 <baesae@hotmail.ch>
2017-05-01 16:32:28 +0200

    Corrected semantics of strncmp

--- lib/stdlibc/string.c
+++ lib/stdlibc/string.c
@@ -52,13 +52,13 @@ int strcmp(const char *str1, const char *str2)
 int strncmp(const char *str1, const char *str2, size_t size)
 {
 	size_t i;
-	for(i = 0; str1[i] != '\0' && str2[i] != '\0' && i < size; i++)
+	for(i = 0; i < size && str1[i] != '\0' && str2[i] != '\0'; i++)
 	{
 		if(str1[i] == str2[i])
 			continue;
 		return str1[i] - str2[i];
 	}
-	if(str1[i] == str2[i])
+	if(i >= size || str1[i] == str2[i])
 		return 0;
 	return str1[i] - str2[i];
 }
