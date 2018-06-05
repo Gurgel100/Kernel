5c82b3ba545cf758b9cf1836e34b021ffc6923d3
Gurgel100 <baesae@hotmail.ch>
2018-06-05 19:25:25 +0200

    Adjusted sys/types.h header to use the global types

--- include/sys/types.h
+++ include/sys/types.h
@@ -8,19 +8,19 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
-#include <stdint.h>
+#include <bits/types.h>
 
-typedef uint64_t clock_t;
-typedef uint64_t id_t;
-typedef uint64_t ino_t;
-typedef uint64_t pid_t;
-typedef unsigned long size_t;
-typedef uint64_t time_t;
+typedef _clock_t clock_t;
+typedef _uint64_t id_t;
+typedef _uint64_t ino_t;
+typedef _pid_t pid_t;
+typedef _size_t size_t;
+typedef _time_t time_t;
 
-typedef uint64_t uid_t;
-typedef uint64_t gid_t;
-typedef uint64_t mode_t;
-typedef uint64_t nlink_t;
-typedef size_t off_t;
+typedef _uint64_t uid_t;
+typedef _uint64_t gid_t;
+typedef _uint64_t mode_t;
+typedef _uint64_t nlink_t;
+typedef _size_t off_t;
 
 #endif /* TYPES_H_ */
