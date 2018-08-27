383ba194e30e374578bf4630d5e3afa4a5deaf98
Gurgel100 <baesae@hotmail.ch>
2018-08-27 03:42:44 +0200

    Added new system panic functionality

--- util/system.c
+++ util/system.c
@@ -7,8 +7,17 @@
 
 #include "system.h"
 #include "pmm.h"
+#include "console.h"
+#include "stdio.h"
+#include "debug.h"
+#include "lock.h"
+#include "scheduler.h"
 
 extern uint64_t Uptime;
+
+char system_panic_buffer[25 * 80];
+static lock_t panic_buffer_lock = LOCK_UNLOCKED;
+
 /*
  * Speichert Systeminformationen in die übergebene Struktur
  * Parameter:	Adresse auf die Systeminformationen-Struktur
@@ -18,4 +27,20 @@ void getSystemInformation(SIS *Struktur)
 	Struktur->physSpeicher = pmm_getTotalPages() * 4096;
 	Struktur->physFree = pmm_getFreePages() * 4096;
 	Struktur->Uptime = Uptime;
+}
+
+void system_panic_enter()
+{
+	lock(&panic_buffer_lock);
+}
+
+void system_panic()
+{
+	console_switch(0);
+	printf("\e[37;41mSystem panic\t\t");
+	if(currentThread != NULL)
+		printf("process: %lu(%.20s), tid: %lu", currentProcess->PID, currentProcess->cmd, currentThread->tid);
+	printf("\n\nThe following error occurred:\n%s\n", system_panic_buffer);
+	asm volatile("cli;hlt");
+	__builtin_unreachable();
 }
--- util/system.h
+++ util/system.h
@@ -10,6 +10,10 @@
 
 #include <bits/sys_types.h>
 
+extern char system_panic_buffer[25 * 80];
+
 void getSystemInformation(SIS *Struktur);
+void system_panic_enter();
+void system_panic() __attribute__((noreturn));
 
 #endif /* SYSTEM_H_ */
